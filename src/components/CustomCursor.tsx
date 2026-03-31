ee4efeaea533ade3ecf5658139373396eb22656b
JohnSang16 <johnsang1970@gmail.com>
2026-03-30 23:29:58 -0400

    fixed mouse responsiveness

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react'
-import { motion, useMotionValue, useSpring } from 'framer-motion'
+import { motion, useMotionValue } from 'framer-motion'
 
 export default function CustomCursor() {
   const mouseX = useMotionValue(-100)
   const mouseY = useMotionValue(-100)
   const [isPointer, setIsPointer] = useState(false)
-
-  const springX = useSpring(mouseX, { stiffness: 500, damping: 30 })
-  const springY = useSpring(mouseY, { stiffness: 500, damping: 30 })
 
   useEffect(() => {
     const move = (e: MouseEvent) => {
@@ -23,7 +20,7 @@ export default function CustomCursor() {
   return (
     <motion.div
       className="fixed top-0 left-0 z-[9999] pointer-events-none"
-      style={{ x: springX, y: springY }}
+      style={{ x: mouseX, y: mouseY }}
     >
       <motion.img
         src="/cursor/lol-cursor.png"
